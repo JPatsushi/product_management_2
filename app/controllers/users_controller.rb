febf37afc7e48e7923d8ad13371f56a1fe8881bd
Watanabe Atsushi <atsushi20180516@gamil.com>
2018-08-30 09:59:47 +0000

    standard_config2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :index]
   
   def index
     if params[:q]
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
       @q = User.ransack(activated_true: true)
       # @users = User.paginate(page: params[:page])
       # @users = User.where(activated: true).paginate(page: params[:page])
-      @title = "ユーザー"
+      @title = "ユーザ一覧"
     end
       @users = @q.result.page(params[:page])
   end
